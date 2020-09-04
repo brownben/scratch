9a73396c6f7aaf5b0bd625e2667dfa733824679c
brownben <9870007+brownben@users.noreply.github.com>
2020-09-04 21:06:05 +0100

    Feature - Lists
    - Add Support for Lists
    - Add support for rendering nested elements

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -5,21 +5,32 @@ interface props {
   input: string
 }
 
-const outputTag = (tag: Tag, index: number) => {
+const processTags = (tags: Tag[], level: string) =>
+  tags.map((tag, index) => outputTag(tag, index, level))
+
+const outputTag = (tag: Tag, index: number, level: string) => {
   const TagName = tag.tag as keyof JSX.IntrinsicElements
-  if (tag.body) return <TagName key={index}>{tag.body}</TagName>
-  else return <TagName key={index} />
+  const key = `${level}-${index}`
+  if (!tag.body) return <TagName key={key} />
+  else if (typeof tag.body === 'string')
+    return <TagName key={key}>{tag.body}</TagName>
+  else
+    return (
+      <TagName key={key}>
+        {processTags(tag.body, `${level}-${index}-${tag.tag}`)}
+      </TagName>
+    )
 }
 
 export default ({ input }: props) => {
-  const processedOutput = parser(input)
+  const abstractSyntaxTree = parser(input)
 
   return (
     <article className="flex-grow w-full h-full md:mt-6">
       <h2 className="my-4 font-sans text-2xl font-bold">Output:</h2>
       <hr className="mb-4" />
       <div className="mx-auto prose max-w-none md:prose-lg">
-        {processedOutput.map(outputTag)}
+        {processTags(abstractSyntaxTree, 'root')}
       </div>
     </article>
   )
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
 export interface Tag {
   tag: string
-  body?: string
+  body: string | Tag[]
 }
 
 export const parser = (input: string): Tag[] => {
@@ -8,13 +8,27 @@ export const parser = (input: string): Tag[] => {
   const lines: string[] = input.split('\n')
 
   let remainingBody = ''
+  let currentWrapper: Tag | null = null
+
   for (const line of lines) {
     const {
       isHeading,
       isBlockquote,
       isHorizontalRule,
+      isOrderedListItem,
+      isUnorderedListItem,
       flushPrevious,
     } = checkLine(line)
+
+    console.log(isOrderedListItem, line)
+
+    if (
+      (currentWrapper && !isOrderedListItem && currentWrapper.tag !== 'ol') ||
+      (currentWrapper && !isUnorderedListItem && currentWrapper.tag !== 'ul')
+    ) {
+      tags.push(currentWrapper)
+      currentWrapper = null
+    }
 
     if (flushPrevious && remainingBody) {
       tags.push({ tag: 'p', body: remainingBody })
@@ -25,10 +39,39 @@ export const parser = (input: string): Tag[] => {
       tags.push({ tag: `h${isHeading[1].length}`, body: isHeading[2] })
     else if (isBlockquote)
       tags.push({ tag: `blockquote`, body: isBlockquote[1] })
-    else if (isHorizontalRule) tags.push({ tag: `hr` })
+    else if (isHorizontalRule) tags.push({ tag: `hr`, body: '' })
+    else if (
+      isOrderedListItem &&
+      currentWrapper?.tag === 'ol' &&
+      Array.isArray(currentWrapper.body)
+    )
+      currentWrapper.body.push({
+        tag: 'li',
+        body: isOrderedListItem[1],
+      })
+    else if (
+      isUnorderedListItem &&
+      currentWrapper?.tag === 'ul' &&
+      Array.isArray(currentWrapper.body)
+    )
+      currentWrapper.body.push({
+        tag: 'li',
+        body: isUnorderedListItem[1],
+      })
+    else if (isUnorderedListItem)
+      currentWrapper = {
+        tag: 'ul',
+        body: [{ tag: 'li', body: isUnorderedListItem[1] }],
+      }
+    else if (isOrderedListItem)
+      currentWrapper = {
+        tag: 'ol',
+        body: [{ tag: 'li', body: isOrderedListItem[1] }],
+      }
     else remainingBody += line
   }
 
+  if (currentWrapper) tags.push(currentWrapper)
   if (remainingBody) tags.push({ tag: 'p', body: remainingBody })
 
   return tags
@@ -36,13 +79,17 @@ export const parser = (input: string): Tag[] => {
 
 const matchesHeading = (line: string) => line.match(/^(#+)\s+(.*)$/)
 const matchesBlockquote = (line: string) => line.match(/^>\s+(.*)$/)
+const matchesHorizontalRule = (line: string) => line.match(/^---$/)
+const matchesOrderedListItem = (line: string) => line.match(/^[0-9]+\.\s+(.*)$/)
+const matchesUnorderedListItem = (line: string) => line.match(/^-\s+(.*)$/)
 const matchesBlank = (line: string) => line.match(/^\s*$/)
-const matchesHorizontalRule = (line: string) => line.match(/^\s*---\s*$/)
 
 interface LineSummary {
   isHeading: RegExpMatchArray | null
   isBlockquote: RegExpMatchArray | null
   isHorizontalRule: RegExpMatchArray | null
+  isOrderedListItem: RegExpMatchArray | null
+  isUnorderedListItem: RegExpMatchArray | null
   isBlank: RegExpMatchArray | null
   flushPrevious?: RegExpMatchArray | null
 }
@@ -51,11 +98,18 @@ const checkLine = (line: string) => {
     isHeading: matchesHeading(line),
     isBlockquote: matchesBlockquote(line),
     isHorizontalRule: matchesHorizontalRule(line),
+    isOrderedListItem: matchesOrderedListItem(line),
+    isUnorderedListItem: matchesUnorderedListItem(line),
     isBlank: matchesBlank(line),
   } as LineSummary
 
   data.flushPrevious =
-    data.isHeading || data.isBlockquote || data.isBlank || data.isHorizontalRule
+    data.isHeading ||
+    data.isBlockquote ||
+    data.isHorizontalRule ||
+    data.isOrderedListItem ||
+    data.isUnorderedListItem ||
+    data.isBlank
 
   return data
 }
