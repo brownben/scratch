0415d949073fddb6174eb955cb6940be2535a5f9
brownben <9870007+brownben@users.noreply.github.com>
2020-09-04 20:04:35 +0100

    Feature - Horizontal Rule

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -7,7 +7,8 @@ interface props {
 
 const outputTag = (tag: Tag, index: number) => {
   const TagName = tag.tag as keyof JSX.IntrinsicElements
-  return <TagName key={index}>{tag.body}</TagName>
+  if (tag.body) return <TagName key={index}>{tag.body}</TagName>
+  else return <TagName key={index} />
 }
 
 export default ({ input }: props) => {
@@ -17,7 +18,9 @@ export default ({ input }: props) => {
     <article className="flex-grow w-full h-full md:mt-6">
       <h2 className="my-4 font-sans text-2xl font-bold">Output:</h2>
       <hr className="mb-4" />
-      <div className="prose md:prose-lg">{processedOutput.map(outputTag)}</div>
+      <div className="mx-auto prose max-w-none md:prose-lg">
+        {processedOutput.map(outputTag)}
+      </div>
     </article>
   )
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
 export interface Tag {
   tag: string
-  body: string
+  body?: string
 }
 
 export const parser = (input: string): Tag[] => {
@@ -9,11 +9,14 @@ export const parser = (input: string): Tag[] => {
 
   let remainingBody = ''
   for (const line of lines) {
-    const isHeading = matchesHeading(line)
-    const isBlockquote = matchesBlockquote(line)
-    const isBlank = matchesBlank(line)
+    const {
+      isHeading,
+      isBlockquote,
+      isHorizontalRule,
+      flushPrevious,
+    } = checkLine(line)
 
-    if ((isHeading || isBlockquote || isBlank) && remainingBody) {
+    if (flushPrevious && remainingBody) {
       tags.push({ tag: 'p', body: remainingBody })
       remainingBody = ''
     }
@@ -22,6 +25,7 @@ export const parser = (input: string): Tag[] => {
       tags.push({ tag: `h${isHeading[1].length}`, body: isHeading[2] })
     else if (isBlockquote)
       tags.push({ tag: `blockquote`, body: isBlockquote[1] })
+    else if (isHorizontalRule) tags.push({ tag: `hr` })
     else remainingBody += line
   }
 
@@ -33,3 +37,25 @@ export const parser = (input: string): Tag[] => {
 const matchesHeading = (line: string) => line.match(/^(#+)\s+(.*)$/)
 const matchesBlockquote = (line: string) => line.match(/^>\s+(.*)$/)
 const matchesBlank = (line: string) => line.match(/^\s*$/)
+const matchesHorizontalRule = (line: string) => line.match(/^\s*---\s*$/)
+
+interface LineSummary {
+  isHeading: RegExpMatchArray | null
+  isBlockquote: RegExpMatchArray | null
+  isHorizontalRule: RegExpMatchArray | null
+  isBlank: RegExpMatchArray | null
+  flushPrevious?: RegExpMatchArray | null
+}
+const checkLine = (line: string) => {
+  const data = {
+    isHeading: matchesHeading(line),
+    isBlockquote: matchesBlockquote(line),
+    isHorizontalRule: matchesHorizontalRule(line),
+    isBlank: matchesBlank(line),
+  } as LineSummary
+
+  data.flushPrevious =
+    data.isHeading || data.isBlockquote || data.isBlank || data.isHorizontalRule
+
+  return data
+}
