a0ed6ccdedd0a1a4556c40226429de30cd4dadea
brownben <9870007+brownben@users.noreply.github.com>
2020-09-06 16:50:43 +0100

    Fix - Make More Similar to GFM

--- src/parser.ts
+++ src/parser.ts
@@ -3,6 +3,8 @@ import { inlineParser } from './inlineParser'
 export interface Tag {
   tag: string
   body: string | Tag[]
+  length?: number
+  character?: string
 }
 
 export class Parser {
@@ -25,8 +27,7 @@ export class Parser {
 
   addHeading(isHeading: RegExpMatchArray) {
     const level = isHeading[1].length
-    if (level <= 6) this.addTag(`h${level}`, isHeading[2])
-    else this.addTag('p', isHeading[2])
+    this.addTag(`h${level}`, isHeading[2])
   }
   addBlockquote(isBlockquote: RegExpMatchArray) {
     this.addTag('blockquote', isBlockquote[1])
@@ -46,12 +47,18 @@ export class Parser {
     else if (this.currentWrapper)
       this.currentWrapper.body = [{ tag: 'li', body: inlineParser(item) }]
   }
-  openCodeBlock() {
-    this.currentWrapper = { tag: 'pre', body: [] }
+  openCodeBlock(match: RegExpMatchArray) {
+    this.currentWrapper = {
+      tag: 'pre',
+      body: [],
+      character: match[1],
+      length: match[2].length + 1,
+    }
   }
   addCodeBlockLine(line: Line) {
-    if (line.isCodeBlock()) this.closeCodeBlock()
-    else this.remainingBody += `${line}\n`
+    const isEnding = line.isCodeBlockEnding(this.currentWrapper)
+    if (isEnding) this.closeCodeBlock()
+    else this.remainingBody += `${line.getRaw()}\n`
   }
   closeCodeBlock() {
     if (this.currentWrapper) {
@@ -109,7 +116,7 @@ export class Parser {
         this.addList('ul', isUnorderedListItem[1])
       else if (isOrderedListItem) this.addList('ol', isOrderedListItem[1])
       else if (isUnorderedListItem) this.addList('ul', isUnorderedListItem[1])
-      else if (isCodeBlock) this.openCodeBlock()
+      else if (isCodeBlock) this.openCodeBlock(isCodeBlock)
       else this.remainingBody += line.getRaw()
     }
 
@@ -130,13 +137,13 @@ class Line {
   }
 
   isHeading() {
-    return this.line.match(/^(#+)\s+(.*)$/)
+    return this.line.match(/^\s{0,3}[^\\](#{1,6})\s+(.*)\s*[^\\#*]#*\s*$/)
   }
   isBlockquote() {
-    return this.line.match(/^>\s+(.*)$/)
+    return this.line.match(/^\s{0,3}[^\\]>\s+(.*)\s*$/)
   }
   isHorizontalRule() {
-    return this.line.match(/^---$/)
+    return this.line.match(/^\s{0,3}([-_*])\s*(\1\s*){2,}\s*$/)
   }
   isOrderedListItem() {
     return this.line.match(/^[0-9]+\.\s+(.*)$/)
@@ -145,7 +152,17 @@ class Line {
     return this.line.match(/^-\s+(.*)$/)
   }
   isCodeBlock() {
-    return this.line.match(/^```(.*)\s*$/)
+    return this.line.match(/^([`~])(\1{2,})(.*)\s*$/)
+  }
+  isCodeBlockEnding(currentWrapper: Tag | null) {
+    const match = this.line.match(/^([`~])(\1{2,})([A-Za-z0-9]*)\s*$/)
+    if (currentWrapper)
+      return (
+        match &&
+        currentWrapper?.character === match[1] &&
+        (currentWrapper?.length ?? 3) <= match[2].length
+      )
+    else return match
   }
   isBlank() {
     return this.line.match(/^\s*$/)
